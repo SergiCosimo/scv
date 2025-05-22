b2da3088e236abaf06a964134d1fb0e0329390b4
Cosimo <sergi.cosimo23@gmail.com>
2025-05-22 19:35:53 +0200

    modifiche

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SergiCosimo/', // nome repo GitHub
+  base: '/scv', // nome repo GitHub
   plugins: [react()],
 })
